29e7ff9b687188ce185415e5287bccf363dd7349
Christian Cruz <christiancruzarango@gmail.com>
2020-05-19 15:00:16 -0500

    actualizacion serve

--- routes/router.ts
+++ routes/router.ts
@@ -1,9 +1,12 @@
-
 import { Router, Request, Response } from 'express';
+import Server from '../classes/server';
 
 const router = Router();
 
-
+/*mandar un mensaje priovado se instancia la instancia del serve y como es un patron singelton 
+*ya la instancia viene cuando se conecta el servidor
+*/
+const server = Server.instance;
 
 router.get('/mensajes', ( req: Request, res: Response  ) => {
 
@@ -19,6 +22,15 @@ router.post('/mensajes', ( req: Request, res: Response  ) => {
     const cuerpo = req.body.cuerpo;
     const de     = req.body.de;
 
+    const payload ={
+        de,
+        cuerpo
+    }  
+
+    /*para enviar mensajes privado a todas las personas 
+    */
+   server.io.emit('mensaje-nuevo',payload);
+
     res.json({
         ok: true,
         cuerpo,
@@ -33,6 +45,19 @@ router.post('/mensajes/:id', ( req: Request, res: Response  ) => {
     const cuerpo = req.body.cuerpo;
     const de     = req.body.de;
     const id     = req.params.id;
+
+    const payload ={
+        de,
+        cuerpo
+    }  
+
+    /*Se llama el servidor de socket que se encuentra en el io,donde se llama el in
+    *que sirve para llamar a una persona que se encuentra en un canal particular,esto realiza el llamada
+    a una sala que hace referencia al mismo id de la persona quien recibe el mensaje
+    */
+    server.io.in(id).emit('mensaje-privado',payload);
+
+  
 
     res.json({
         ok: true,
